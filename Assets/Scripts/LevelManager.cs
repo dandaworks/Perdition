7d285e8420a25d957ad99990434b0909fbc5cb8a
John Connor <jckusho@gmail.com>
2025-07-30 23:23:15 -0400

    Update `Hub`, `LevelManager.cs`

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using piqey.Utilities.Editor;
 
 public class LevelManager : MonoBehaviour
 {
@@ -9,40 +10,37 @@ public class LevelManager : MonoBehaviour
     public bool chooseScene = false;
     public bool timer;
     public float waitTime = 5f;
-    public int chooseSceneIndex = 0;
+
+    [Tooltip("The scene that should be loaded if ChooseScene is toggled On")]
+    public SceneField SceneToLoad;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (timer)
-        {
-            StartCoroutine("Wait", waitTime);
-        }
+            StartCoroutine(nameof(Wait), waitTime);
     }
+
     void OnTriggerEnter(Collider other)
     {
         if (onCollide && other.CompareTag(playerTag))
-        {
             LoadScene();
-        }
     }
+
     IEnumerator Wait(float timer)
     {
         yield return new WaitForSeconds(waitTime);
         LoadScene();
     }
+
     public void LoadScene()
     {
         if (!chooseScene)
-        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
         else
-        {
-            SceneManager.LoadScene(chooseSceneIndex);
-        }
+            SceneManager.LoadScene((string)SceneToLoad);
     }
-    public void ExitGame()
-    {
+
+    public void ExitGame() =>
         Application.Quit();
-    }
 }
